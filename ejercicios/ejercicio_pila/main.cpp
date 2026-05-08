0bc94c47f3e2ebaa67207eea7417d2c61661c1d6
Jza26 <a25216345@alumnos.uady.mx>
2026-05-08 12:50:38 -0600

    mi pila, ejercicio

---
+++ ejercicios/ejercicio_pila/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "custom_stack.h"       
+
+int main() {
+    CustomStack mi_pila;
+    mi_pila.push(1);
+    std::cout<<"mi_pila.top: "<<mi_pila.top()<<std::endl;
+
+
+    return 0;
+
+}
